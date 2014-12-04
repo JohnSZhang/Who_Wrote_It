695d4dfeab88e8f9cd4783a9dbc2cde6e3af9a19
John Zhang <john.shimin.zhang@gmail.com>
2014-12-04 18:41:28 -0500

    fill out question model a bit

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,27 @@
 class Question
-  attr_accessor :answered
+  attr_accessor :answered, :sentence, :book_id
 
   def initialize
     self.answered = false
+    self.authors = []
+    self.authors = Hash.new
+  end
 
+  def pick_sentence
+    self.sentence = Sentence.random()
+    author = self.sentence.author
+    self.authors[author.name] = author.id
+    self.book_id = self.sentence.book_id
   end
 
   def get_choices
-    
+    remaining = Author.all.where.not(:id, self.sentence.author.id)
+    remaining.sample(3).each do |author|
+      self.authors[author.name] = author.id
+    end
   end
 
-  def check_answer
+  def check_answer(idx)
     self.answered = true
 
   end
