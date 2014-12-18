aa927c1135e2ce515e1f65def4dd494f7ac1069c
John Zhang <john.shimin.zhang@gmail.com>
2014-12-18 14:44:38 -0800

    remove some more unnecessary specs

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,24 +3,12 @@ require 'rails_helper'
 describe "CMS Pages" do
 	describe "static pages" do
 
-		subject { page }
-
 		describe "Home Page" do
 			before { visit root_path }
-			it { is_expected.to have_title('Who Wrote It Home')}
-			it { is_expected.to have_content('Take a Quiz!') }
-			it { is_expected.to have_link ('Quiz')}
-			it { is_expected.to have_link ('About')}
-			it { is_expected.to have_link ('Books')}
+			it { is_expected.to have_title('Who Wrote It')}
+			it { is_expected.to have_content('Welcome') }
+			it { is_expected.to have_link ('Start')}
 		end
 
-		describe "About Page" do
-			before { visit about_path }
-			it { is_expected.to have_title('Who Wrote It About')}
-			it { is_expected.to have_content('Why did hulk') }
-			it { is_expected.to have_link ('Quiz')}
-			it { is_expected.to have_link ('About')}
-			it { is_expected.to have_link ('Books')}
-		end
 	end
 end
